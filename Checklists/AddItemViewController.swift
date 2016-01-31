cd94047d607436098797145023653dcdd2fad409
Hyung jin Jun <junhyungjin@me.com>
2016-02-01 00:29:43 +0900

    disable done bar button

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,9 +9,10 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
@@ -30,5 +31,17 @@ class AddItemViewController: UITableViewController {
         super.viewWillAppear(animated)
         textField.becomeFirstResponder()
     }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
 
+        if newText.length > 0 {
+            doneBarButton.enabled = true
+        } else {
+            doneBarButton.enabled = false
+        }
+        return true
+    }
 }
