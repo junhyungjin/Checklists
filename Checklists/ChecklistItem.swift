944f71403325050c24fc628bb41a2a010f1a2267
Hyung jin Jun <junhyungjin@me.com>
2016-02-26 00:29:46 +0900

    save file...fix crash

--- Checklists/ChecklistItem.swift
+++ Checklists/ChecklistItem.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 
-class ChecklistItem: NSObject {
+class ChecklistItem: NSObject, NSCoding {
     var text = ""
     var checked = false
     
     func toggleChecked() {
         checked = !checked
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(text, forKey: "Text")
+        aCoder.encodeBool(checked, forKey: "Checked")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+    }
+    
+    override init() {
+        super.init()
     }
 }
